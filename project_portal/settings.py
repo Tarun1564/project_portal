cba8788b00ee718d04bfd023dd738f1daec98212
Kondaka Tarun Sai <113244858+Tarun1564@users.noreply.github.com>
2026-04-14 10:54:41 +0530

    Update settings.py

--- project_portal/settings.py
+++ project_portal/settings.py
@@ -152,3 +152,4 @@ EMAIL_USE_TLS = True
 EMAIL_HOST_USER = 'kondakatarunsai@gmail.com'
 EMAIL_HOST_PASSWORD = 'xmcwgrxuhxgejgow'
 DEFAULT_FROM_EMAIL = EMAIL_HOST_USER
+CSRF_TRUSTED_ORIGIN='https://projectportal-production.up.railway.app/'
